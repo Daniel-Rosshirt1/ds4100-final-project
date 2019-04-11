4efbae92ecdb91871f099e0df3f93002cacf03c0
aliyah8 <tyshkanbayeva.a@husky.neu.edu>
2019-04-11 02:06:31 -0400

    web scrapping to pull the standings data

--- DS4100_finalproject.r
+++ DS4100_finalproject.r
@@ -53,7 +53,111 @@ nba
 
 
 
+################################################### TEAMS DATA ##################################################################
+## WEB SCRAPPING WITH RVEST TO PULL THE STANDINGS DATA
+library(rvest)
 
+page <- read_html("https://www.basketball-reference.com/leagues/NBA_2019_standings.html?fbclid=IwAR0xnccmjlJRX5qZBCeSbIF_ubY_PlMo9kMmzKwEvMnjoX4mq5UPA0Tvi_k#all_confs_standings_E")
+
+cast <- page %>%
+  html_nodes("#fs-sidewall-right-container , .center , .left , .right") %>%
+  html_text()
+cast
+
+
+easternteams <- page %>%
+  html_nodes("#fs-sidewall-right-container , #confs_standings_E .left") %>%
+  html_text()
+easternteams
+easternteams
+
+easternW <- page %>%
+  html_nodes("#fs-sidewall-right-container , #confs_standings_E .left+ .right , #confs_standings_E .left+ .center") %>%
+  html_text()
+easternW
+easternW
+
+easternL <- page %>%
+  html_nodes("#fs-sidewall-right-container , #confs_standings_E .right:nth-child(3) , #confs_standings_E .center+ .center") %>%
+  html_text()
+easternL
+easternL
+
+
+easternWL <- page %>% 
+  html_nodes("#fs-sidewall-right-container , #confs_standings_E .right:nth-child(4)") %>%
+  html_text()
+easternWL 
+easternWL
+length(easternWL)
+
+
+easternGB <- page %>%
+  html_nodes("#fs-sidewall-right-container , #confs_standings_E .right:nth-child(5)") %>%
+  html_text()
+easternGB
+easternGB
+length(easternGB)
+
+easternPSG<-page %>%
+  html_nodes("#confs_standings_E .right:nth-child(6)") %>%
+  html_text()
+easternPSG
+easternPSG
+
+
+easternPAG <- page %>%
+  html_nodes("#confs_standings_E .right:nth-child(7)") %>%
+  html_text()
+easternPAG
+easternPAG
+
+easternSRS <- page %>%
+  html_nodes("#confs_standings_E .right:nth-child(7)") %>%
+  html_text()
+easternSRS
+easternSRS
+
+nba_info <- data.frame(easternteams, easternW, easternL, easternGB, easternPSG, easternPAG, easternSRS)
+length_adress <- length(nba_info)
+westernNames <- page %>%
+  html_nodes("#confs_standings_W .full_table .left") %>%
+  html_text()
+westernNames
+
+
+westernW <- page %>%
+  html_nodes("#confs_standings_W .left+ .right") %>%
+  html_text()
+westernW
+
+westernL <- page %>%
+  html_nodes("#confs_standings_W .right:nth-child(3)") %>%
+  html_text()
+westernL
+
+westernGB <- page %>%
+  html_nodes("#confs_standings_W .full_table .right:nth-child(4)")%>%
+  html_text()
+westernGB
+
+westernPSG <- page %>%
+  html_nodes("#confs_standings_W .full_table .right:nth-child(6)")%>%
+  html_text()
+westernPAG
+
+westernPAG <- page %>%
+  html_nodes("#confs_standings_W .full_table .right:nth-child(7)")%>%
+  html_text()
+westernPAG
+
+
+westernSRS <- page %>%
+  html_nodes("#confs_standings_W .right:nth-child(8)")%>%
+  html_text()
+westernPAG
+
+nba_info <- data.frame(westernNames, westernW, westernL, westernGB, westernPSG, westernPAG, westernSRS)
 
 
 
